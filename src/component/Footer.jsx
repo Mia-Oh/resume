33899efd6f1ba89d667f1d8003718d34f2933c76
Mia-Oh <songmiaoh@gmail.com>
2023-04-12 18:44:42 +0900

    feat:Footer 생성

---
+++ src/component/Footer.jsx
@@ -0,0 +1,19 @@
+import { SFooterLayout } from "../style/style";
+
+const Footer = () => {
+  return (
+    <SFooterLayout>
+      <p>
+        <em>감사합니다.</em>
+      </p>
+      <sub>
+        <sup>
+        Front-End Engineer, 
+        <a href="https://github.com/Mia-Oh" target="_blank" rel="noreferrer">@Mia-Oh</a>
+        </sup>
+      </sub>
+    </SFooterLayout>    
+  );
+};
+
+export default Footer;
