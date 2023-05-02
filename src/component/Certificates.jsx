be40db28da78e684616a089a905a0c672a408e44
Mia-Oh <songmiaoh@gmail.com>
2023-05-02 15:58:10 +0900

    feat:Certificates-파일 추가

---
+++ src/component/Certificates.jsx
@@ -0,0 +1,40 @@
+import { SSubtitle,SContent,SPeriodSection,SExperienceInfoSection,SExperienceDescription } from "../style/style";
+
+const Certificates = () => {
+  return (
+    <>
+      <section>
+        <SSubtitle>Certificates</SSubtitle>
+        <SContent>
+          <SPeriodSection>
+            <span>2022.04</span>
+          </SPeriodSection>
+          <SExperienceInfoSection>
+            <SExperienceDescription>SMAT 서비스 경영 3급</SExperienceDescription>
+            <p>한국생산성본부</p>
+          </SExperienceInfoSection>
+        </SContent>
+        <SContent>
+          <SPeriodSection>
+            <span>2015.12</span>
+          </SPeriodSection>
+          <SExperienceInfoSection>
+            <SExperienceDescription>ERP 물류 정보관리사 2급</SExperienceDescription>
+            <p>한국생산성본부</p>
+          </SExperienceInfoSection>
+        </SContent>
+        <SContent>
+          <SPeriodSection>
+            <span>2015.06</span>
+          </SPeriodSection>
+          <SExperienceInfoSection>
+            <SExperienceDescription>ERP 회게 정보관리사 2급</SExperienceDescription>
+            <p>한국생산성본부</p>
+          </SExperienceInfoSection>
+        </SContent>
+
+      </section>
+    </>
+  )
+};
+export default Certificates;
